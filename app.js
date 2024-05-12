b4cb17b7f2112c1d6f94267d3d935717d77a5f3b
ZubairrRiaz <160521830+ZubairrRiaz@users.noreply.github.com>
2024-05-12 05:33:45 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,74 @@
+import inquirer from "inquirer";
+let idNumber = 1000;
+let studentList = [];
+let studentFees = 5000;
+let condition = true;
+while (condition) {
+    let asnw = await inquirer.prompt([{
+            name: 'num0',
+            type: 'list',
+            message: 'What you want to do?',
+            choices: ['Enroll Student', 'Check Student status']
+        }]);
+    if (asnw.num0 == 'Check Student status') {
+        let res = await inquirer.prompt({
+            name: 'num4',
+            type: 'input',
+            message: 'Enter Name to find Student'
+        });
+        let findName = res.num4.trim();
+        let found = studentList.find(studentList => studentList.name === findName);
+        if (found == undefined) {
+            console.log('Student Not Found!');
+        }
+        else {
+            console.log(found);
+        }
+        break;
+    }
+    if (asnw.num0 == 'Enroll Student') {
+        let ans = await inquirer.prompt([
+            {
+                name: 'num1',
+                type: 'input',
+                message: 'Enter your name?'
+            },
+        ]);
+        let studentName = ans.num1.trim();
+        if (studentName === '') {
+            console.log(`Enter Something`);
+        }
+        else {
+            let ans1 = await inquirer.prompt({
+                name: 'num3',
+                type: 'list',
+                message: 'Which course do you want to enroll?',
+                choices: ['Artificial intelligence', 'Web 3', 'Metaverse', 'BlockChain']
+            });
+            class Student {
+                name;
+                StudentId;
+                CourseFees;
+                courseENroll = [];
+                constructor(name, StudentId, CourseFees) {
+                    this.name = name;
+                    this.StudentId = StudentId;
+                    this.CourseFees = CourseFees;
+                    this.courseENroll.push(ans1.num3);
+                }
+            }
+            let Zubair = new Student(studentName, idNumber++, studentFees);
+            studentList.push(Zubair);
+            console.log(studentList);
+        }
+        let answer = await inquirer.prompt({
+            name: "num3",
+            type: 'confirm',
+            default: true,
+            message: 'Do you want to continue?'
+        });
+        if (answer.num3 == false) {
+            condition = false;
+        }
+    }
+}
